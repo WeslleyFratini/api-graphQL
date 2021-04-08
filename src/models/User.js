a6dff4c74ad940ccdc36bb27b27486de635fa59d
WeslleyFratini <weeslleey354@gmail.com>
2021-04-07 22:03:46 -0300

    config mongoose and create models

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const Schema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lasttName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+export default mongoose.model("User", Schema);
